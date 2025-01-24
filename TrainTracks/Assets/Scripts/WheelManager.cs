79382dee319f7d48d1e3bb33d1f036c4522f0c2d
ATTOCORRECT <97545662+ATTOCORRECT@users.noreply.github.com>
2025-01-23 21:28:57 -0500

    done

--- TrainTracks/Assets/Scripts/WheelManager.cs
+++ TrainTracks/Assets/Scripts/WheelManager.cs
@@ -6,17 +6,20 @@ public class WheelManager : MonoBehaviour
 {
     public GameObject Engine;
     RailEngineManager EngineManager;
-    public Animator Rolling;
+    public GameObject LeftWheel;
+    public GameObject RightWheel;
+    float speed = 5000;
     // Start is called before the first frame update
     void Start()
     {
-        Rolling.SetFloat("Multiplier", 0);
         EngineManager = Engine.GetComponent<RailEngineManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Rolling.SetFloat("Multiplier", Mathf.Lerp(EngineManager.targetVelocity, EngineManager.velocity, .5f) * 50);
+        float rotationalVelocity = Mathf.Lerp(EngineManager.targetVelocity, EngineManager.velocity, .5f) * speed * Time.deltaTime;
+        LeftWheel.transform.Rotate(Vector3.forward * rotationalVelocity * -1);
+        RightWheel.transform.Rotate(Vector3.forward * rotationalVelocity);
     }
 }
